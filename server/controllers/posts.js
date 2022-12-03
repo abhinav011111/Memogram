1e3ec52c2b2fb5ecc74e6f102e21448c7ea566cc
MOHD. UMAM <umam20@iitk.ac.in>
2022-12-03 15:04:57 +0530

    comment-added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,22 +1,51 @@
 import PostMessage from "../models/postMessage.js";
+
 // Get posts
-export const getPosts = async(req, res) => {
+export const getPosts = async (req, res) => {
+
+    // Here we have used async-await function
+    // try block is initiated when there is no error
+    // catch is used for error
     try {
-        const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages);
-           
-    } catch (error) {
-        res.status(404).json({mesage:error.message});
         
+        // We use await and is use find() function to return an array of all the posts;
+        const postMessages = await PostMessage.find();
+        /*
+            Response
+                status code : 200
+                body : array of all posts
+        */
+        res.status(200).json(postMessages);
+    } catch (error) {
+        /*
+            Response
+                status code : 404
+                body : error message
+        */
+        res.status(404).json({ mesage: error.message });
     }
 };
 
-export const createPost = async(req, res) => {
-    const post=req.body;
-    const newPost=new PostMessage(post);
+
+// To create a new post
+export const createPost = async (req, res) => {
+
+    // contains the req body....
+    const post = req.body;
+    // creating a new post
+    const newPost = new PostMessage(post);
+
+
     try {
+
+        // save it haha.
         await newPost.save();
     } catch (error) {
-        res.status(409).json({mesage:error.message});
+        /*
+            Response
+                status code : 409
+                body : error message
+        */
+        res.status(409).json({ mesage: error.message });
     }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,6 @@
 import express from 'express'; // importing express
 
+// post also imported
 import {getPosts, createPost} from '../controllers/posts.js'; // importing handler for the get method of the post router
 
 // Creating router for the express server
@@ -9,6 +10,8 @@ const router = express.Router();
 // the request is sent to '/' 
 // but due to the prefix it goes to '/posts'
 router.get('/', getPosts);
+
+// post controller
 router.post('/', createPost);
 
 // exporting default router
