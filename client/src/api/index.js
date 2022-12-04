cdf6ba27b9d4f21735a5315b3e4d681a1ed42a69
MOHD. UMAM <umam20@iitk.ac.in>
2022-12-04 11:00:42 +0530

    axios-add

---
+++ client/src/api/index.js
@@ -0,0 +1,11 @@
+/*
+    Axios is a promised-based HTTP client for JavaScript. It has the ability to make HTTP requests from the browser and handle the transformation of request and response data.
+*/
+import axios from 'axios';
+
+// URL on which the backend send the posts.
+const url = 'http://localhost:5000/posts';
+
+// Creating a fetchPost function to retrieve the posts
+// using the axios.get() function.
+export const fetchPost = () => axios.get(url);
