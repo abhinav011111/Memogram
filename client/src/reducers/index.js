a70fe3f511cfe9a57525b599ebcacb27ac25f26f
Suhani97 <suhani.agrawal.ece20@itbhu.ac.in>
2022-12-10 21:10:51 +0530

    google login-logout added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux"; // To combine all the reducers
 
 // importing posts
 import posts from './posts';
+import auth from './auth';
 
 // combine the reducer posts
-export default combineReducers({ posts }); // this is a reducer
+export default combineReducers({ posts, auth }); // this is a reducer
